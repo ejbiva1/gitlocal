105abf24cafc5e2e8a3c1e9355b2ba856b843f47
dazzle666 <516384487@qq.com>
2018-10-18 20:02:53 +0800

    持仓判断

--- util/Trader.py
+++ util/Trader.py
@@ -1,20 +1,36 @@
+from decimal import Decimal
+from util.Calculator import Calculator
+
+
 class Trader:
     # judge position status
     @staticmethod
-    def position_judge(position, strategy_id):
-        if position == True:
-            return True
-        else:
-            return False
-    #
-    # @staticmethod
-    # def buy(cost, amount, position):
-    #     if Trader.position_judge(position):
-    #         # todo  do buy
-    #         return 1
-    #     else:
-    #         return 0
-    #
-    # @staticmethod
-    # def sell(price, amount, postion):
-    #     return 0
+    def position_judge(position, strategy_id, price, trade_amount, timestamp):
+        # buy_b strategy ==3
+        if strategy_id == 3:
+            if position.coin_cash_rate >= 0.8:
+                return False
+            elif position.coin_cash_rate <= 0.75:
+                amount = Decimal(Decimal('0.05') * Decimal(position.total))
+                signal = 1
+                calculator = Calculator(position, timestamp, price, amount, signal, strategy_id, 1)
+                calculator.buy(price)
+                # todo 回写数据库交易成功字段
+            #     sell_b == 4
+        elif strategy_id == 4:
+            signal = 0
+            calculator = Calculator(position, timestamp, price, trade_amount, signal, strategy_id, 1)
+            calculator.sell(price)
+            # todo 回写数据库交易成功字段
+        elif strategy_id == 2:
+            signal = 0
+            calculator = Calculator(position, timestamp, price, trade_amount, signal, strategy_id, 1)
+            calculator.sell(price)
+            # todo 回写数据库交易成功字段
+        elif strategy_id == 1:
+            signal = 1
+            calculator = Calculator(position, timestamp, price, trade_amount, signal, strategy_id, 1)
+            calculator.sell(price)
+            # todo 回写数据库交易成功字段
+
+
