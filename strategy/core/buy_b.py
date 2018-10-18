4ed2222cb090894c48eeda37ce9d58a6d8e209c7
dazzle666 <516384487@qq.com>
2018-10-18 20:01:22 +0800

    buy_B

--- strategy/core/buy_b.py
+++ strategy/core/buy_b.py
@@ -1,16 +1,18 @@
-from util.ReadData import read_datas
-import decimal
+from util.ReadData import read_datas_60min
+from decimal import Decimal
+from util.Trader import *
 
 
 class BuyB:
     # 初始化策略参数
-    def __init__(self, start_time, end_time, balance):
+    def __init__(self, start_time, end_time, position):
         self.start_time = start_time
         self.end_time = end_time
-        self.balance = balance
-        self.init_balance = balance
-        self.datas = read_datas(start_time, end_time).sort_values(by='id', axis=0, ascending=True)
+        # self.balance = balance
+        # self.init_balance = balance
+        self.datas = position.datas
         self.idt = self.datas['id']
+        self.position = position
 
     def condition_1(self, T):
         timestamp = T - 7200
@@ -77,9 +79,20 @@ class BuyB:
         pre_2T = T - 7200
         df1 = self.datas[(self.idt == pre_T)]
         df2 = self.datas[(self.idt == pre_2T)]
-        vol2 = decimal.Decimal(df2.iat[0, 6]) * decimal.Decimal('0.75')
+        vol2 = Decimal(df2.iat[0, 6]) * Decimal('0.75')
         # HIGH(T+1)>=OPEN(T-1) - [OPEN(T-1)-CLOSE(T-1)]10%
         if df1.iat[0, 6] < vol2:
+            return True
+        else:
+            return False
+
+    def condition_a(self, T):
+        pre_T = T - 3600
+        df = self.datas[(self.idt == pre_T)]
+        df_t = self.datas[(self.idt == T)]
+        price = Decimal(df.iat[0, 1] - df.iat[0, 2]) / Decimal('2') + df.iat[0, 2]
+        # HIGH(T+1)>=OPEN(T-1) - [OPEN(T-1)-CLOSE(T-1)]10%
+        if df_t.iat[0, 3] < price:
             return True
         else:
             return False
@@ -87,24 +100,30 @@ class BuyB:
     def strategy(self, T):
         if (self.condition_1(T) and self.condition_2(T)
                 and self.condition_3(T) and self.condition_4(T)
-                and self.condition_5(T) and self.condition_6(T)):
+                and self.condition_5(T) and self.condition_6(T)
+                and self.condition_a(T)):
             # send signal
+            pre_T = T - 3600
+            df = self.datas[(self.idt == pre_T)]
+            price = Decimal(df.iat[0, 1] - df.iat[0, 2]) / Decimal('2') + Decimal(df.iat[0, 2])
+            Trader.position_judge(position=self.position, strategy_id=3, price=price, timestamp=T, trade_amount=0)
+            # return True
+        else:
+            # 没有买卖操作
+            calculator = Calculator(self.position, T, price=0, amount=0, signal=0, strategy_id=3, strategy_account_id=1)
+            calculator.non_trade()
+    # def run_strategy(self):
+    #     df = self.datas
+    #     for timestamp in df['id']:
+    #         return self.strategy(timestamp)
 
-            return True
-
-    def run_strategy(self):
-        df = self.datas
-        for timestamp in df['id']:
-            return self.strategy(timestamp)
-
-
-if __name__ == '__main__':
-    buyB = BuyB(1508990400, 1509001200, 200)
-    print(buyB.idt)
-    # sellB = SellB(1508997600, 1508990400, 1511481600, 200)
-    # print(sellB.filter())
-    # print(sellB.condition_filter())
-    # print(sellB.test())
-    # print(sellB.condition_1())
-    # print(sellB.condition_b(1508994000))
-    # print(buyB.run_strategy())
+# if __name__ == '__main__':
+# buyB = BuyB(1508990400, 1509001200, 200)
+# print(buyB.idt)
+# sellB = SellB(1508997600, 1508990400, 1511481600, 200)
+# print(sellB.filter())
+# print(sellB.condition_filter())
+# print(sellB.test())
+# print(sellB.condition_1())
+# print(sellB.condition_b(1508994000))
+# print(buyB.run_strategy())
