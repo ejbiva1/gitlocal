c7fedba0bc3a6f445451a1702755d6c259a92354
stanley <ejbiva@163.com>
2018-10-22 10:47:51 +0800

    增加三个新的方法

--- web/app/controller.py
+++ web/app/controller.py
@@ -2,12 +2,12 @@ import DB
 
 def getStrategy():
     return DB.getStrategy()
-
+#启动策略
 def startStartStrategy():
     return ''
-
+#查询历史列表
 def loadLogList(creator):
     return DB.getStrategyLogList(creator)
-
+#查询历史账户列表
 def getLogDetail(stratgyLogId):
     return DB.getLogDetail(stratgyLogId)
