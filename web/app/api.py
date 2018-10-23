68e2c1aa3633cd4ec2993b1398c4ed3c5b1f8732
rdai2 <rui.dai@dxc.com>
2018-10-23 11:25:24 +0800

    1. 使用 Flask 中 make_response 在 返回数据中添加 Response 返回对象

--- web/app/api.py
+++ web/app/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, Response
+from flask import Flask, request, session, make_response
 import DB as db
 import json
 import os
@@ -18,15 +18,14 @@ def startStrategy():
     initBalance = request.json.get("initBalance")
     coinCategory = request.json.get("coinCategory")
     if initBalance <= 1000000 and initBalance >= 10000:
-        controller.startStartStrategy(strategyId,initBalance,startTime,endTime)
-        return Response(json.dumps({'result': 'strategy started'}))
+        controller.startStartStrategy(strategyId, initBalance, startTime, endTime)
+        return make_response(json.dumps({'result': 'strategy started'}))
     else:
-        return Response(json.dumps({'result': 'pls be sure initBalance is correct'}))
+        return make_response(json.dumps({'result': 'pls be sure initBalance is correct'}))
 
 
 @app.route('/loadLogList', methods=['POST'])
 def loadLogList():
-    # 后端写死一个 session 对象，保存一个session['userId']
 
     strategy_log_list = []
     strategyLogList = controller.loadLogList(session['userId'])
@@ -34,7 +33,10 @@ def loadLogList():
         strategy_log_list.append(item.__dict__)
     print(strategy_log_list)
     result = json.dumps({"list": strategy_log_list}, ensure_ascii=False, cls=JsonExtendEncoder)
-    return Response(result)
+    response = make_response(result)
+    response.status = "200"
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 
 @app.route('/getLogDetail/<int:strategyLogId>', methods=['post'])
@@ -42,39 +44,12 @@ def getLogDetail(strategyLogId):
     session.permanent = True
     session['userId'] = 1
     log_details = controller.getLogDetail(strategyLogId, session['userId'])
-    # for item in log_details_list:
-    #     print(item.__dict__)
-    #     log_details.append(item.__dict__)
+
     result = json.dumps({'list': log_details.__dict__}, ensure_ascii=False, cls=JsonExtendEncoder)
-
-    return Response(result)
-
-
-@app.route('/getStrategyInstanceList', methods=['post', 'put'])
-def getStrategyInstanceList():
-    strategy_list = []
-    strategyList = controller.getStrategy()
-    for item in strategyList:
-        # print(item.__dict__)
-        strategy_list.append(item.__dict__)
-
-    # json.dumps 序列化时对中文默认使用的ascii编码 : json.dumps 序列化对象时 对中文默认使用 ascii编码
-    result = json.dumps({"list": strategy_list}, cls=JsonExtendEncoder, ensure_ascii=False)
-    return Response(result)
-
-
-# 给URL 添加变量部分: 规则可以用 <converter:variable_name> 指定一个可选的转换器
-@app.route('/getStrategyInstance/<int:strategy_id>', methods=['post', 'put'])
-def getStrategyInstance(strategy_id):
-    if request.method == 'post':
-        strategyInstance = db.getStrategyInstance(strategy_id)
-        # strategyInstance_list = []
-        # strategyInstance_list.append(strategyInstance.__dict__)
-        # result = json.dumps({'result': strategyInstance_list}, ensure_ascii=False, cls=JsonExtendEncoder)
-
-        # python 对象 转化成 json 字符串
-        result = json.dumps({'result': strategyInstance.__dict__}, ensure_ascii=False, cls=JsonExtendEncoder)
-        return Response(result)
+    response = make_response(result)
+    response.status = "200"
+    response.headers["Content-Type"] = "application/json"
+    return result
 
 
 if __name__ == "__main__":
