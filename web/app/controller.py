1ca8d987cb13bc59edbfc4968e71f001e23b874e
stanley <ejbiva@163.com>
2018-10-24 11:07:41 +0800

    add new folders

--- web/app/controller.py
+++ web/app/controller.py
@@ -1,10 +1,19 @@
 import DB
+import sys
+sys.path.append("../..")
+import strategy.main as strategyTool
+
+
 
 def getStrategy():
     return DB.getStrategy()
 #启动策略
 def startStartStrategy(strategyId,initBalance,startDate,endDate):
-    return ''
+    if (strategyId==1):
+        strategyTool.strategy_combination_a(startDate,endDate,initBalance)
+    elif (strategyId==2):
+        strategyTool.strategy_combination_b(startDate, endDate, initBalance)
+    return "好像启动了"
 #查询历史列表
 def loadLogList(creator):
     # creator 是指用户的用户名； 需要我创建一个session
