42fd6e771ecef215c042782cbbd7d868253812f7
rdai2 <rui.dai@dxc.com>
2018-11-06 11:29:29 +0800

    1. api: saveStrategyConf 完成 2. 修改对应sql 以及 database

--- web/app/api.py
+++ web/app/api.py
@@ -3,8 +3,8 @@ import DB as db
 import json
 import os
 import web.app.controller as controller
+from strategy import main
 from facilties.functional import JsonExtendEncoder
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
@@ -26,8 +26,9 @@ def startStrategy():
 
 @app.route('/loadLogList', methods=['POST'])
 def loadLogList():
-
     strategy_log_list = []
+    session.permanent = True
+    session['userId'] = 1
     strategyLogList = controller.loadLogList(session['userId'])
     for item in strategyLogList:
         strategy_log_list.append(item.__dict__)
@@ -50,6 +51,39 @@ def getLogDetail(strategyLogId):
     response.status = "200"
     response.headers["Content-Type"] = "application/json"
     return result
+
+
+@app.route('/saveStrategyConf', methods=['post'])
+def saveStrategyConf():
+    session.permanent = True
+    session['userId'] = 1
+    # strategyId,userId,initBalance,startDate,endDate,strategyConfItemlist
+    strategyId = request.json.get("strategyId")
+    # print(strategy_Id)
+    startDate = request.json.get("startDate")
+    # print(start_Date)
+    endDate = request.json.get("endDate")
+    # print(end_Date)
+    initBalance = request.json.get("initBalance")
+    # print(init_Balance)
+    strategyConfItemlist = request.json.get("strategyConfItemlist")
+    coin_category = request.json.get("kind")
+
+    controller.saveStrategyConf(strategyId=strategyId, userId=session['userId'], startDate=startDate,
+                                endDate=endDate,
+                                initBalance=initBalance, strategyConfItemlist=strategyConfItemlist)
+    regression_result = main.strategy_poc(strategy_id=strategyId, user_id=session['userId'],
+                                          coin_category=coin_category,
+                                          start_time=startDate, end_time=endDate, init_balance=initBalance)
+    print(regression_result.__dict__)
+
+    response = make_response(
+        json.dumps({'result': regression_result.__dict__}, ensure_ascii=False, cls=JsonExtendEncoder))
+    response = make_response(response)
+    response.status = "200"
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 
 if __name__ == "__main__":
--- web/app/controller.py
+++ web/app/controller.py
@@ -1,28 +1,36 @@
 import DB
 import sys
+
 sys.path.append("../..")
 import strategy.main as strategyTool
 
 
-
 def getStrategy():
     return DB.getStrategy()
-#启动策略
-def startStartStrategy(strategyId,initBalance,startDate,endDate):
-    if (strategyId==1):
-        strategyTool.strategy_combination_a(startDate,endDate,initBalance)
-    elif (strategyId==2):
+
+
+# 启动策略
+def startStartStrategy(strategyId, initBalance, startDate, endDate):
+    if (strategyId == 1):
+        strategyTool.strategy_combination_a(startDate, endDate, initBalance)
+    elif (strategyId == 2):
         strategyTool.strategy_combination_b(startDate, endDate, initBalance)
     return "好像启动了"
-def saveStrategyConf(strategyId,userId,initBalance,startDate,endDate,strategyConfItemlist):
-    strategyConfId = DB.saveStrategyConf(userId,strategyId)
-    for item in strategyConfItemlist:
-        DB.saveStrategyConfItem(item.strategy_conf_id,item.index_label,item.formular,item.price)
 
-#查询历史列表
+
+def saveStrategyConf(strategyId, userId, initBalance, startDate, endDate, strategyConfItemlist):
+    strategyConfId = DB.saveStrategyConf(userId, strategyId)
+    for item in strategyConfItemlist:
+        DB.saveStrategyConfItem(item['strategy_conf_id'], item['index_label'], item['formular'],
+                                item['price'], item['direction'])
+
+
+# 查询历史列表
 def loadLogList(creator):
     # creator 是指用户的用户名； 需要我创建一个session
     return DB.getStrategyLogList(creator)
-#查询历史账户列表
+
+
+# 查询历史账户列表
 def getLogDetail(stratgyLogId, creator):
     return DB.getLogDetail(stratgyLogId, creator)
