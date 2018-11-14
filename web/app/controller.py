9cdb0c7771d6dfeb1af58a3ee028923819a0717c
ejbiva1 <ejbiva@163.com>
2018-11-14 16:22:35 +0800

    Update controller.py

--- web/app/controller.py
+++ web/app/controller.py
@@ -14,13 +14,39 @@ def startStartStrategy(strategyId,initBalance,startDate,endDate):
     elif (strategyId==2):
         strategyTool.strategy_combination_b(startDate, endDate, initBalance)
     return "好像启动了"
-def saveStrategyConf(strategy_name,userId,coin_category,initBalance,startDate,endDate,strategyConfItemlist):
+
+#检查是否有重名的策略，True已存在，False不存在
+def checkStrategyName(strategy_name):
+    return DB.checkStrategyName(strategy_name)
+
+#保存策略并执行策略
+def saveStrategyAndRun(strategy_name,userId,coin_category,initBalance,startDate,endDate,strategyConfItemlist):
+    result ={'status':0,'error_message':"","strategy_id":0}
+    if (DB.checkStrategyName(strategy_name)):
+        result['status']=-1
+        result['error_message']="策略名称重复"
+        return result
     strategyId = DB.saveStrategy(strategy_name,userId,coin_category,initBalance,startDate,endDate)
-    print(strategyId)
     for item in strategyConfItemlist:
-        DB.saveStrategyConfItem(strategyId,item['index_label'],item['formular'],item['price'], item['direction'])
-    return strategyTool.strategy_poc(strategyId, startDate, endDate, initBalance)
-   # strategy_id, start_time, end_time, init_balance
+        DB.saveStrategyConfItem(item['strategy_id'],item['index_label'],item['formular'],item['price'])
+    strategyTool.strategy_poc(strategyId, userId, startDate, endDate, initBalance)
+    result['strategy_id'] = strategyId
+    return result
+
+#保存策略
+def saveStrategy(strategy_name,userId,coin_category,initBalance,startDate,endDate,strategyConfItemlist):
+    result ={'status':0,'error_message':"","strategy_id":0}
+    if (DB.checkStrategyName(strategy_name)):
+        result['status']=-1
+        result['error_message']="策略名称重复"
+        return result
+    strategyId = DB.saveStrategy(strategy_name,userId,coin_category,initBalance,startDate,endDate)
+    for item in strategyConfItemlist:
+        DB.saveStrategyConfItem(item['strategy_id'],item['index_label'],item['formular'],item['price'])
+
+    result['strategy_id'] = strategyId
+    return result
+
 #查询历史列表
 def loadLogList(creator):
     # creator 是指用户的用户名； 需要我创建一个session
@@ -28,3 +54,6 @@ def loadLogList(creator):
 #查询历史账户列表
 def getLogDetail(stratgyLogId, creator):
     return DB.getLogDetail(stratgyLogId, creator)
+
+
+
