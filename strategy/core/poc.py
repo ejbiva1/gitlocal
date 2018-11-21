ac276dade623a33ddc1f67b292783b277a49afa7
lovesh1130 <yijunw@hpe.com>
2018-11-21 11:12:24 +0800

    增加技术指标close(T) open(T) high(T) low(T)

--- strategy/core/poc.py
+++ strategy/core/poc.py
@@ -7,17 +7,23 @@ from decimal import Decimal
 
 def sell_signal(t, sell_dict, data):
     df = data[data['id'] == t]
-    s1 = close_t_1(df, sell_dict)
-    s2 = open_t_1(df, sell_dict)
-    s3 = high_t_1(df, sell_dict)
-    s4 = low_t_1(df, sell_dict)
-    s5 = close_t_2(df, sell_dict)
-    s6 = open_t_2(df, sell_dict)
-    s7 = high_t_2(df, sell_dict)
-    s8 = low_t_2(df, sell_dict)
+    df_1 = data[data['id'] == t - 86400]
+    df_2 = data[data['id'] == t - 172800]
+    s1 = close_t_1(df_1, sell_dict)
+    s2 = open_t_1(df_1, sell_dict)
+    s3 = high_t_1(df_1, sell_dict)
+    s4 = low_t_1(df_1, sell_dict)
+    s5 = close_t_2(df_2, sell_dict)
+    s6 = open_t_2(df_2, sell_dict)
+    s7 = high_t_2(df_2, sell_dict)
+    s8 = low_t_2(df_2, sell_dict)
+    s11 = close_t(df, sell_dict)
+    s12 = close_t(df, sell_dict)
+    s13 = close_t(df, sell_dict)
+    s14 = close_t(df, sell_dict)
     s9 = ma5_t(data, sell_dict, t)
     s10 = ma10_t(data, sell_dict, t)
-    if s1 and s2 and s3 and s4 and s5 and s6 and s7 and s8 and s9 and s10:
+    if s1 and s2 and s3 and s4 and s5 and s6 and s7 and s8 and s9 and s10 and s11 and s12 and s13 and s14:
         # todo 卖出信号
         price = df.iat[0, 2]
         signal = Poc_signal(signal=2, price=Decimal(price).quantize(Decimal('0.000000')))
@@ -28,17 +34,23 @@ def sell_signal(t, sell_dict, data):
 
 def buy_signal(t, buy_dict, data):
     df = data[data['id'] == t]
-    b1 = close_t_1(df, buy_dict)
-    b2 = open_t_1(df, buy_dict)
-    b3 = high_t_1(df, buy_dict)
-    b4 = low_t_1(df, buy_dict)
-    b5 = close_t_2(df, buy_dict)
-    b6 = open_t_2(df, buy_dict)
-    b7 = high_t_2(df, buy_dict)
-    b8 = low_t_2(df, buy_dict)
+    df_1 = data[data['id'] == t - 86400]
+    df_2 = data[data['id'] == t - 172800]
+    b1 = close_t_1(df_1, buy_dict)
+    b2 = open_t_1(df_1, buy_dict)
+    b3 = high_t_1(df_1, buy_dict)
+    b4 = low_t_1(df_1, buy_dict)
+    b5 = close_t_2(df_2, buy_dict)
+    b6 = open_t_2(df_2, buy_dict)
+    b7 = high_t_2(df_2, buy_dict)
+    b8 = low_t_2(df_2, buy_dict)
+    b11 = close_t(df, buy_dict)
+    b12 = close_t(df, buy_dict)
+    b13 = close_t(df, buy_dict)
+    b14 = close_t(df, buy_dict)
     b9 = ma5_t(data, buy_dict, t)
     b10 = ma10_t(data, buy_dict, t)
-    if b1 and b2 and b3 and b4 and b5 and b6 and b7 and b8 and b9 and b10:
+    if b1 and b2 and b3 and b4 and b5 and b6 and b7 and b8 and b9 and b10 and b11 and b12 and b13 and b14:
         # todo 买入信号
         price = df.iat[0, 2]
         signal = Poc_signal(signal=1, price=Decimal(price).quantize(Decimal('0.000000')))
@@ -111,6 +123,42 @@ def open_t_1(df, dict):
 
 
 def close_t_1(df, dict):
+    if dict['close(T-1)'] is not 0:
+        x = df.iat[0, 2]
+        flag = eval(dict['close(T-1)'])
+        return flag
+    else:
+        return True
+
+
+def low_t(df, dict):
+    if dict['low(T-1)'] is not 0:
+        x = df.iat[0, 3]
+        flag = eval(dict['low(T-1)'])
+        return flag
+    else:
+        return True
+
+
+def high_t(df, dict):
+    if dict['high(T-1)'] is not 0:
+        x = df.iat[0, 4]
+        flag = eval(dict['high(T-1)'])
+        return flag
+    else:
+        return True
+
+
+def open_t(df, dict):
+    if dict['open(T-1)'] is not 0:
+        x = df.iat[0, 1]
+        flag = eval(dict['open(T-1)'])
+        return flag
+    else:
+        return True
+
+
+def close_t(df, dict):
     if dict['close(T-1)'] is not 0:
         x = df.iat[0, 2]
         flag = eval(dict['close(T-1)'])
