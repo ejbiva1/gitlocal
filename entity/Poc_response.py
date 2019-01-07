467125ab9efc4b471b4b39e4a4edc0fe28942dc8
dazzle666 <516384487@qq.com>
2019-01-07 16:04:48 +0800

    poc返回log_id

--- entity/Poc_response.py
+++ entity/Poc_response.py
@@ -1,6 +1,5 @@
 class Poc_response:
-    def __init__(self, strategy_profit, benchmark_profit):
+    def __init__(self, strategy_profit, benchmark_profit, log_id):
         self.strategy_profit = strategy_profit
         self.benchmark_profit = benchmark_profit
-
-
+        self.strategy_log_id = log_id
